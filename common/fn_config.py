f86834926945600944c00b9df9dc2acf55acaee3
jaepil-choi <chljeffreyz@gmail.com>
2024-05-26 16:59:09 +0900

    긴 자산이름 대신 축약어 사용하도록 수정 (예: 'KOSEF 단기자금': 'shortterm')

---
+++ common/fn_config.py
@@ -0,0 +1,26 @@
+ASSET_ALIASES = {
+    'KOSEF 단기자금': 'shortterm',
+    'KOSEF 미국달러선물': 'usd',
+    'KOSEF 미국달러선물인버스': 'usdinv',
+    'KODEX 인버스': 'kodexinv',
+    'TIGER 원유선물Enhanced(H)': 'oil',
+    'KODEX 골드선물(H)': 'gold',
+    'TIGER 단기선진하이일드(합성 H)': 'hybond',
+    'KBSTAR 중기우량회사채': 'midbond',
+    'KOSEF 국고채10년': '10y',
+    'TIGER 차이나CSI300': 'csi300',
+    'ACE 일본Nikkei225(H)': 'nikkei',
+    'TIGER 유로스탁스50(합성 H)': 'euro50',
+    'TIGER 미국S&P500선물(H)': 'sp500',
+    'TIGER 코스닥150': 'kosdaq150',
+    'KODEX 200': 'kodex200'
+}
+
+ITEM_ALIASES = {
+    '거래대금(원)': 'moneyvolume',
+    '수익률(%)': 'return',
+    '수정고가(원)': 'adjhigh',
+    '수정시가(원)': 'adjopen',
+    '수정저가(원)': 'adjlow',
+    '수정주가(원)': 'adjclose'
+}
